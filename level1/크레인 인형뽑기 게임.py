c768ef933cc47b7fb4b1d48c56aca23b2a4c2e5b
gun8474 <gun8474@gmail.com>
2021-04-15 20:48:16 +0900

    크레인 인형뽑기 게임

--- level1/크레인 인형뽑기 게임.py
+++ level1/크레인 인형뽑기 게임.py
@@ -22,8 +22,10 @@ def solution(board, moves):
                         answer = answer + 2
     print(answer)
     return answer
+
+## 예시
 moves = [1,5,3,5,1,2,1,4]
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-a = solution(board, moves)
-a
+solution(board, moves)
+
 
